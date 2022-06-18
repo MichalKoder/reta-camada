5a5d26cf34429123f6e4f35e159b98e08bb6cd92
Michal Sapinski <Sapinski.Michal@gmail.com>
2022-06-18 11:14:57 +0200

    Add Role table related to Candidate table to SQL script

--- app/Database/reta_dashboard_db.sql
+++ app/Database/reta_dashboard_db.sql
@@ -1,12 +1,21 @@
+DROP DATABASE reta_camada;
 CREATE DATABASE reta_camada
 USE reta_camada;
-GRANT ALL ON reta_camada.* TO 'username'@'localhost' IDENTIFIED BY 'password';
+-- GRANT ALL ON reta_camada.* TO 'username'@'localhost' IDENTIFIED BY 'password';
+
+CREATE TABLE Role (
+    ID INT UNSIGNED NOT NULL AUTO_INCREMENT,
+    name VARCHAR(128),
+    PRIMARY KEY(ID)
+);
 
 CREATE TABLE Candidate (
     ID INT UNSIGNED NOT NULL AUTO_INCREMENT,
     name VARCHAR(128),
     age INT(3) UNSIGNED,
     date_of_application DATE,
-    role VARCHAR(128),
-    PRIMARY KEY(ID)
+    roleID INT UNSIGNED,
+    PRIMARY KEY(ID),
+    FOREIGN KEY(roleID) REFERENCES Role(ID) 
 );
+
